b3a7100e9e35b3b643f74db857176c3f49d1b91e
petrov_maksym <petrov.maxym.v@gmail.com>
2022-02-11 23:53:36 +0200

    last-changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const btnMenu = document.querySelector('.header__btn-menu');
 const menu = document.querySelector('.header__nav');
+const bodyMenuScroll = document.querySelector('body')
 
 btnMenu.addEventListener('click', function(){    
             btnMenu.classList.toggle('header__btn-menu--active');
             menu.classList.toggle('header__nav--open');
+            bodyMenuScroll.classList.toggle('header__nav-scroll')
         })  
 
 
